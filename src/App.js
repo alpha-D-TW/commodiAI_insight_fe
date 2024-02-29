2a3bb42dec22dd09955ac2263f8b12a576daf08b
kyXu <kunyu.xu@thoughtworks.com>
2024-02-29 17:57:25 +0800

    supplementary disclaimer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
-import {Button, ConfigProvider, Divider, Layout} from "antd";
+import {Button, ConfigProvider, Divider, Layout, notification} from "antd";
 import styles from "./components/ChatBot/index.module.scss";
 import SampleQuestions from "./components/ChatBot/SampleQuestions";
 import {isNotEmptyArray} from "./utils/formatter";
@@ -12,6 +12,7 @@ import {onDeleteChat, onGetAllHistory, onGetChatHistoryById, onGetSampleQuestion
 import {CHAT_SESSION_ID, clearSessionStorage, getSessionStorage, setSessionStorage} from "./utils/storage";
 import AllHistoryList from "./components/ChatBot/AllHistoryList";
 import KnowledgeManagement from "./components/KnowledgeManagement";
+import {NotificationPlacement} from "antd/es/notification/interface";
 
 function App() {
     const {Header, Sider, Content} = Layout;
@@ -44,8 +45,35 @@ function App() {
     //         behavior: 'smooth',
     //     });
     // }, [history]);
+    const [api, contextHolder] = notification.useNotification();
 
-
+    const openNotification = (placement: NotificationPlacement) => {
+        const btn = (<Button onClick={() => api.destroy()}>我已阅读相关投资免责声明</Button>)
+        api.warning({
+            style: {
+                width: 680,
+            },
+            btn,
+            message: 'CommodiAI Insight投资免责声明',
+            description: (<>
+                    在使用CommodiAI Insight之前，请您务必仔细阅读以下投资免责声明。<br/>
+                    CommodiAI
+                    Insight提供的信息和建议仅供参考，不构成对特定投资的建议或推荐。投资市场存在风险，可能导致投资本金部分或全部损失。因此，在作出任何投资决策之前，请您仔细评估自身的投资目标、风险承受能力和财务状况。<br/>
+                    <b>信息准确性</b>：尽管我们努力确保CommodiAI
+                    Insight提供的信息准确和可靠，但我们无法保证信息的完整性、及时性和准确性。我们不对因信息不准确或错误而导致的任何损失或损害负责。<br/>
+                    <b>投资建议</b>：CommodiAI Insight提供的投资建议仅基于算法分析和数据处理，不考虑您个人的投资目标、财务状况和风险偏好。任何投资决策应该由您自行负责，我们不承担任何责任<br/>
+                    <b>市场风险</b>：投资市场涉及的风险包括但不限于市场波动、政治事件、经济变化等因素。投资者应该对市场风险有清醒的认识，并做好风险控制和管理。<br/>
+                    <b>技术风险</b>：使用CommodiAI
+                    Insight时可能面临的技术风险包括但不限于互联网连接问题、软件故障等。我们将尽最大努力保障系统的稳定性和安全性，但不对由此造成的任何损失负责。<br/>
+                    <b>免责条款</b>：对于因使用CommodiAI Insight而导致的任何直接或间接损失或损害，包括但不限于利润损失、商业中断或数据丢失，我们不承担任何责任。<br/>
+                    在使用CommodiAI Insight时，请您理性对待投资风险，审慎决策，并在必要时咨询专业的金融顾问或其他资深投资者。任何投资决策均由您自行承担，与CommodiAI
+                    Insight及其相关方无关。
+                </>
+            ),
+            placement,
+            duration: 0,
+        });
+    };
     useEffect(() => {
         const fetchSampleQuestions = async () => {
             const data = await onGetSampleQuestions(language === LANGUAGES.EN)
@@ -81,7 +109,7 @@ function App() {
         }
         setIsError(false);
         setLoading(true);
-        const res = await onPostChat(question, currentSessionId, GEN_AI_MODEL_TYPE.GPT35TURBO, existingQuestionId);
+        const res = await onPostChat(question, currentSessionId, GEN_AI_MODEL_TYPE.WENXIN, existingQuestionId);
         if (res?.id) {
             setCurrentSessionId(res.id);
             setHistory(res.history);
@@ -203,12 +231,15 @@ function App() {
                                     isLoading={loading}
                                     questions={_.flatten(_.map(sampleQuestions, item => item?.questions))}
                                 />
-                                <div style={{
-                                    fontSize: '12px',
-                                    color: '#555',
-                                    lineHeight: '32px',
-                                    textAlign: 'center'
-                                }}>免责声明：在使用CommodiAI Insight之前，请您务必仔细阅读以下投资免责声明。
+                                {contextHolder}
+                                <div
+                                    style={{
+                                        fontSize: '12px',
+                                        color: '#555',
+                                        lineHeight: '32px',
+                                        textAlign: 'center'
+                                    }}>免责声明：在使用CommodiAI Insight之前，请您务必仔细阅读以下
+                                    <a onClick={() => openNotification('topRight')}>投资免责声明</a>。
                                 </div>
                             </div>
                         </div>) : (<KnowledgeManagement/>)}
--- src/components/ChatMessage/TopBar.js
+++ src/components/ChatMessage/TopBar.js
@@ -1,49 +1,45 @@
-import { Button, Tooltip } from 'antd';
-import copy from 'copy-to-clipboard';
-import { useTranslation } from 'react-i18next';
-
-import { ReactComponent as InternalIcon } from '../../icons/chat-internal.svg';
-import { ReactComponent as CopyIcon } from '../../icons/chat-copy.svg';
-import { ReactComponent as RefreshIcon } from '../../icons/refresh.svg';
-import { ReactComponent as ErrorIcon } from '../../icons/error-circle.svg';
+import {Button, Tooltip} from 'antd';
+import {useTranslation} from 'react-i18next';
+import {ReactComponent as RefreshIcon} from '../../icons/refresh.svg';
+import {ReactComponent as ErrorIcon} from '../../icons/error-circle.svg';
 import styles from './TopBar.module.scss';
 
-const TopBar = ({ isError, isLastOne, onRefresh, isStrAnswer, answer }) => {
-  const isAvailableStrAnswer = isStrAnswer && answer;
-  const { t } = useTranslation();
-  return (
-    <div className={styles.topBar}>
-      {isError ? (
-        <div className={styles.errorLabel}>
-          <ErrorIcon />
-          <span>{t('label.chat.answer_type.error')}</span>
+const TopBar = ({isError, isLastOne, onRefresh, isStrAnswer, answer}) => {
+    const isAvailableStrAnswer = isStrAnswer && answer;
+    const {t} = useTranslation();
+    return (
+        <div className={styles.topBar}>
+            {isError ? (
+                <div className={styles.errorLabel}>
+                    <ErrorIcon/>
+                    <span>{t('label.chat.answer_type.error')}</span>
+                </div>
+            ) : (
+                <div>
+                    <Button>👍</Button>
+                    <Button>👎</Button>
+                </div>
+                // <div className={styles.label}>
+                //   <InternalIcon />
+                //   <span>{t('label.chat.answer_type.internal')}</span>
+                // </div>
+            )}
+            <div className={styles.actionBar}>
+                {isLastOne && (
+                    <Tooltip title={t('operation.refresh_answer')}>
+                        <Button icon={<RefreshIcon className={styles.refreshIcon}/>} onClick={onRefresh}/>
+                    </Tooltip>
+                )}
+                {/*<Tooltip title={t('operation.copy')}>*/}
+                {/*  <Button*/}
+                {/*    disabled={!isAvailableStrAnswer}*/}
+                {/*    icon={<CopyIcon />}*/}
+                {/*    onClick={() => (isAvailableStrAnswer ? copy(answer) : null)}*/}
+                {/*  />*/}
+                {/*</Tooltip>*/}
+            </div>
         </div>
-      ) : (
-          <div>
-              <Button>👍</Button>
-              <Button>👎</Button>
-          </div>
-        // <div className={styles.label}>
-        //   <InternalIcon />
-        //   <span>{t('label.chat.answer_type.internal')}</span>
-        // </div>
-      )}
-      <div className={styles.actionBar}>
-        {isLastOne && (
-          <Tooltip title={t('operation.refresh_answer')}>
-            <Button icon={<RefreshIcon className={styles.refreshIcon} />} onClick={onRefresh} />
-          </Tooltip>
-        )}
-        {/*<Tooltip title={t('operation.copy')}>*/}
-        {/*  <Button*/}
-        {/*    disabled={!isAvailableStrAnswer}*/}
-        {/*    icon={<CopyIcon />}*/}
-        {/*    onClick={() => (isAvailableStrAnswer ? copy(answer) : null)}*/}
-        {/*  />*/}
-        {/*</Tooltip>*/}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TopBar;
